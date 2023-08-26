1018e36bdb920f7c34bf03cd601004f57cfc6321
Noah Zarro <noahzarro@gmail.com>
2023-08-26 08:54:20 +0200

    added docker file and backup read routes

--- main.py
+++ main.py
@@ -17,16 +17,34 @@ def test():
     return "hello world"
 
 
-@app.route('/backup')
+
+
+@app.route('/backup', methods=['POST', 'GET'])
 def backup():
-    # backup
-    data = request.get_json(force=True)
-    cur_time = datetime.datetime.now()
-    filename = cur_time.strftime("%Y_%m_%d_%H_%M_%S")
-    os.makedirs("backups", exist_ok = True)
-    with open("backups/" + filename + ".json", "w") as f: 
-        json.dump(data, f)
-    return ("", 204)
+    if request.method == 'GET':
+        # get the names of all backup files
+        backups = os.listdir("/mount/backups/")
+        return {"backups": backups}
+    if request.method == 'POST':    
+        # save backup
+        data = request.get_json(force=True)
+        cur_time = datetime.datetime.now()
+        filename = cur_time.strftime("%Y_%m_%d_%H_%M_%S")
+        print("saving data:")
+        print(data)
+        os.makedirs("backups", exist_ok = True)
+        with open("/mount/backups/" + filename + ".json", "w") as f: 
+            json.dump(data, f)
+        return ("", 204)
+
+@app.route('/backup/<filename>', methods=['GET'])
+def single_backup(filename):
+    if request.method == 'GET':
+        # get a single backup file
+        backup = {}
+        with open("/mount/backups/" + filename) as f:
+            backup = json.load(f)
+        return {"backup": backup}
 
 if __name__ == '__main__':
     # create backups folder if not present yet
